847a9db13f45912e1632f7aa7280043cd14b6d38
Dmitry Gordeev <nillsondg@gmail.com>
2022-02-20 17:04:36 +0300

    implemented nesting support

--- main.py
+++ main.py
@@ -2,23 +2,23 @@ import glob
 import os
 import re
 from datetime import date
-
 from lxml import etree
 
 # Конвертация словаря Warodai в формат xml, согласно формату
 # https://warodai.ru/about/readme
-# todo 3.4.3. Обозначение омограмм римскими цифрами
-# todo 3.7. Гнездование
+# todo 3.12. Общие отсылки (ср.)
+# подсказки по классам японских символов regex https://gist.github.com/terrancesnyder/1345094
 warodai_dir = "/Users/dmitry/Documents/GitHub/warodai-source/"
 
 
 def convert_warodai_to_xml():
     root = etree.Element("warodai")
     count = 0
+    export_count = 0
     for filename in glob.iglob(warodai_dir + '**/*.txt', recursive=True):
         article = read_article(filename)
         article_name = os.path.basename(filename).split(".")[0]
-        parse_article(article_name, article, root)
+        parse_article(article_name, article, root, export_count)
         count += 1
 
     root = etree.ElementTree(root)
@@ -26,6 +26,7 @@ def convert_warodai_to_xml():
     with open(f'./warodai{today}.xml', 'wb') as f:
         f.write(etree.tostring(root, encoding="UTF-8", pretty_print=True))
     print(f'converted {count} articles')
+    print(f'exported {export_count} articles')
 
 
 def read_article(filename):
@@ -33,25 +34,57 @@ def read_article(filename):
         return f.read()
 
 
-def parse_article(name, article, root):
+def parse_article(name, article, root, export_count):
     print("parse_article " + name)
     split = article.split("\n")
     title = split[0]
     rubrics = split[1:]
-    xml_article = etree.SubElement(root, "article")
-    parse_title(title, xml_article)
-    parse_rubrics(rubrics, xml_article)
 
-
-def parse_title(title, article_element):
     # check https://github.com/warodai/txt_builder/blob/feb8acc7cdda7bb3d29add1c7d98eddb295839f6/builder.php#L196
-    title_match = re.search(r'^([^【(]*)(【([\S ]*)】)?\((.*)\)\s?(\[([ёа-яЁА-Я.:; ]*)\])?\s?〔(\S*)〕', title)
-    kana = title_match.group(1)
-    hyoukies = title_match.group(3)
-    kiriji = title_match.group(4)
-    corpus = title_match.group(6)
-    number = title_match.group(7)
+    title_match = re.search(
+        r'^(?P<kana>[^【(]*)(【(?P<hyoukies>[\S ]*)】)?\((?P<kiriji>.*)\)\s?(\[(?P<corpus>[ёа-яЁА-Я.:; ]*)\])?\s?〔(?P<number>\S*)〕',
+        title)
+    kanas = title_match.group("kana")
+    hyoukies = title_match.group("hyoukies")
+    kirijies = title_match.group("kiriji")
+    corpus = title_match.group("corpus")
+    number = title_match.group("number")
 
+    kana_nesting = kanas.count(", ")
+    hyouki_nesting = hyoukies.count(", ") if hyoukies is not None else 0
+    if 0 < kana_nesting == hyouki_nesting > 0:
+        kana_list = kanas.split(", ")
+        hyouki_list = hyoukies.split(", ")
+        kiriji_list = kirijies.split(", ")
+        for nest_i in range(kana_nesting + 1):
+            kana = kana_list[nest_i]
+            hyouki = hyouki_list[nest_i]
+            kiriji = kiriji_list[nest_i] if len(kiriji_list) > 1 else kiriji_list[0]
+            xml_article = etree.SubElement(root, "article")
+            export_count += 1
+            parse_title(kana, hyouki, kiriji, corpus, number, xml_article)
+            parse_rubrics(rubrics, xml_article)
+    elif kana_nesting > 0 and hyouki_nesting == 0:
+        kana_list = kanas.split(", ")
+        kiriji_list = kirijies.split(", ")
+        for nest_i in range(kana_nesting + 1):
+            kana = kana_list[nest_i]
+            hyouki = hyoukies
+            kiriji = kiriji_list[nest_i] if len(kiriji_list) > 1 else kiriji_list[0]
+            xml_article = etree.SubElement(root, "article")
+            export_count += 1
+            parse_title(kana, hyouki, kiriji, corpus, number, xml_article)
+            parse_rubrics(rubrics, xml_article)
+    elif kana_nesting > 0:
+        raise RuntimeError("kana nesting process failed")
+    else:
+        xml_article = etree.SubElement(root, "article")
+        export_count += 1
+        parse_title(kanas, hyoukies, kirijies, corpus, number, xml_article)
+        parse_rubrics(rubrics, xml_article)
+
+
+def parse_title(kana, hyoukies, kiriji, corpus, number, article_element):
     article_element.set("kana", remove_homogram(kana))
     if hyoukies is not None:
         for hyouki in hyoukies.split("･"):
@@ -82,29 +115,24 @@ def parse_rubrics(rubrics, article_element):
             if litter is not None or litter_kanji != '':
                 litter_match = None
                 if litter is not None:
-                    litter_match = re.match(r'(<i>)([ёЁа-яА-Я ]*\.):?(</i>)', litter)
+                    litter_match = re.match(r'(<i>)(?P<litter>[ёЁа-яА-Я ]*\.):?(</i>)', litter)
                 meaning_with_litter_m = re.search(
-                    r'((<i>)(?P<corpus>[ёЁа-яА-Я. ]*)(</i>))?((?P<deri>: ～[一-龯ぁ-んァ-ン]*)|(?P<kanji>[一-龯ぁ-んァ-ン]*))?(?P<comma>: )?(?P<meaning>.*)$',
+                    r'((<i>)(?P<corpus>[ёЁа-яА-Я;. ]*)(</i>))?((?P<deri>: ～[一-龯ぁ-んァ-ン]*)|(?P<kanji>[一-龯ぁ-んァ-ン]*))?(?P<comma>: )?(?P<meaning>.*)$',
                     meaning)
                 if litter_match is not None and meaning_with_litter_m is not None:
-                    # print(litter_match.groups())
+                    # print(litter_kanji)
                     # print(meaning_with_litter_m.groups())
-                    meaning = " ".join(filter(None, [meaning_with_litter_m.expand(r"\g<deri>"),
-                                                     litter_kanji,
-                                                     litter_match.expand(r"\g<1>\g<2>"),
-                                                     meaning_with_litter_m.expand(r"\g<corpus>\g<comma>"),
-                                                     litter_match.expand(r"\g<3>"),
-                                                     meaning_with_litter_m.expand(r"\g<kanji>"),
-                                                     meaning_with_litter_m.group("meaning")]))
+                    meaning = construct_meaning(meaning_with_litter_m, litter_match, litter_kanji)
                 elif litter is not None:
                     meaning = litter + " " + meaning
                 elif len(litter_kanji) > 0 and meaning_with_litter_m is not None:
-                    meaning = meaning_with_litter_m.expand(r"\g<deri> ") + litter_kanji \
-                              + meaning_with_litter_m.expand(r"\g<1> \g<meaning>")
+                    meaning = meaning_with_litter_m.expand(r"\g<deri> ") + litter_kanji + " " \
+                              + meaning_with_litter_m.expand(r"\g<1> \g<meaning>").strip()
                     meaning = meaning.strip()
                 else:
                     raise RuntimeError("litter process failed")
-            rubric_element.text = meaning
+
+            rubric_element.set("translation", meaning)
             continue
 
         if rubric_element is None:
@@ -129,7 +157,7 @@ def parse_rubrics(rubrics, article_element):
         if rubric_element is None:
             rubric_element = etree.SubElement(article_element, "rubric")
             if derivative_match is None:
-                rubric_element.text = text
+                rubric_element.set("translation", text)
                 continue
 
         if derivative_match is not None:
@@ -137,12 +165,31 @@ def parse_rubrics(rubrics, article_element):
             derivative_element.text = text
             continue
 
+        if idiom_match is not None:
+            idiom_element = etree.SubElement(rubric_element, "idiom")
+            idiom_element.text = text
+            continue
+
         phrase_element = etree.SubElement(rubric_element, "phrase")
         phrase_element.text = text
-        if idiom_match is not None:
-            phrase_element.set("type", "idiom")
-            rubric_element.text = text
-            continue
+
+
+def construct_meaning(meaning_with_litter_m, litter_m, litter_kanji):
+    meaning_litter = meaning_with_litter_m.expand(r"\g<corpus>")
+    litter = litter_m.expand(r"\g<1>")
+    litter += litter_m.expand(r"\g<litter>")
+    if len(meaning_litter) > 0:
+        litter += " " + meaning_litter
+    litter += meaning_with_litter_m.expand(r"\g<comma>").strip()
+    litter += litter_m.expand(r"\g<3>")
+    return " ".join(filter(lambda x: len(x) > 0,
+                           [meaning_with_litter_m.expand(r"\g<deri>"),
+                            litter_kanji,
+                            litter,
+                            meaning_with_litter_m.expand(r"\g<kanji>"),
+                            meaning_with_litter_m.expand(r"\g<meaning>").strip()
+                            ]
+                           ))
 
 
 def clear_line(line):
@@ -157,33 +204,38 @@ def remove_homogram(text):
     homogram_m = re.search(r"([一-龯ぁ-んァ-ンゝ]+)(I+)(.*)", text)
     if homogram_m is None:
         return text
-    return homogram_m.expand(r"\g<1>\g<3>")
+    return homogram_m.expand(r"\g<1>\g<3>").strip()
 
 
 def test_parse():
     test_root = etree.Element("warodai")
     files = [
-        # "004-23-67",
-        # "006-79-94",
-        # "005-75-60",
-        # "008-56-59",
-        # "007-58-00",
-        # "002-52-52",
-        # "005-78-01",
-        # "004-94-80",
-        # "006-07-86",
-        "007-61-80"
+        "004-23-67",
+        "006-79-94",
+        "005-75-60",
+        "008-56-59",
+        "007-58-00",
+        "002-52-52",
+        "005-78-01",
+        "004-94-80",
+        "006-07-86",
+        "007-61-80",
+        "009-26-70",
+        "007-66-47",
+        "007-65-61",
+        "009-12-37"
     ]
+    count = 0
     for filename in files:
         numbers = filename.split("-")
         file = f"{warodai_dir}/{numbers[0]}/{numbers[1]}/{filename}.txt"
         article_name = os.path.basename(file).split(".")[0]
         article = read_article(file)
-        parse_article(article_name, article, test_root)
+        parse_article(article_name, article, test_root, count)
     root = etree.ElementTree(test_root)
     root.write('./warodai_test.xml', encoding="utf-8", pretty_print=True, method="xml")
 
 
 if __name__ == '__main__':
-    convert_warodai_to_xml()
-    # test_parse()
+    # convert_warodai_to_xml()
+    test_parse()
--- parse_tests.py
+++ parse_tests.py
@@ -11,7 +11,7 @@ def test_parse(filename):
     file = f"{warodai_dir}/{numbers[0]}/{numbers[1]}/{filename}.txt"
     article_name = os.path.basename(file).split(".")[0]
     article = read_article(file)
-    parse_article(article_name, article, test_root)
+    parse_article(article_name, article, test_root, 0)
     return etree.ElementTree(test_root)
 
 
@@ -44,28 +44,37 @@ class HyoukiesTestCase(unittest.TestCase):
     #         self.assertEqual("X光線", hyoukies[1].text)
 
 
+class KanaTestsCase(unittest.TestCase):
+    def test_kana1(self):
+        root = test_parse("005-01-17").getroot()
+        for article in root:
+            kana = article.attrib['kana']
+            self.assertEqual("ピッチ", kana)
+
+
 class RubricsTestCase(unittest.TestCase):
     def test_rubric1(self):
         root = test_parse("007-66-47").getroot()
         for article in root:
             rubrics = article.findall('rubric')
-            self.assertEqual("блаженство в нирване", rubrics[0].text)
+            self.assertEqual("блаженство в нирване", rubrics[0][0].text)
 
     def test_rubric2(self):
         root = test_parse("005-95-09").getroot()
         for article in root:
             rubrics = article.findall('rubric')
-            self.assertEqual("<i>см.</i> <a href=\"#008-98-82\">きめこみにんぎょう</a>", rubrics[0].text)
+            self.assertEqual("<i>см.</i> <a href=\"#008-98-82\">きめこみにんぎょう</a>", rubrics[0][0].text)
 
 
 class HomogramsTestCase(unittest.TestCase):
 
     # todo несостыковочка в документации
+    # ああ, あゝI (а:)〔000-95-65〕
     def test_homogram1(self):
         root = test_parse("000-95-65").getroot()
-        for article in root:
-            kana = article.attrib['kana']
-            self.assertEqual("ああ", kana)
+        self.assertEqual("ああ", root[0].attrib['kana'])
+        self.assertEqual("あゝ", root[1].attrib['kana'])
+        self.assertEqual("а:", root[0].attrib['kiriji'])
 
     def test_homogram2(self):
         root = test_parse("009-07-76").getroot()
@@ -95,82 +104,148 @@ class LittersTestCase(unittest.TestCase):
         root = test_parse("004-23-67").getroot()
         for article in root:
             rubrics = article.findall('rubric')
-            self.assertEqual("<i>ист.</i> императорский двор", rubrics[0].text)
-            self.assertEqual("<i>ист.</i> сёгун", rubrics[1].text)
+            self.assertEqual("<i>ист.</i> императорский двор", rubrics[0][0].text)
+            self.assertEqual("<i>ист.</i> сёгун", rubrics[1][0].text)
 
     # пометы
     def test_litter2(self):
         root = test_parse("006-79-94").getroot()
         for article in root:
             rubrics = article.findall('rubric')
-            self.assertEqual("<i>уст.</i> носильщик паланкина", rubrics[0].text)
-            self.assertEqual("<i>уст. перен. прост.</i> ловкач, продувной парень", rubrics[1].text)
+            self.assertEqual("<i>уст.</i> носильщик паланкина", rubrics[0][0].text)
+            self.assertEqual("<i>уст. перен. прост.</i> ловкач, продувной парень", rubrics[1][0].text)
 
     def test_litter3(self):
         root = test_parse("008-56-59").getroot()
         for article in root:
             rubrics = article.findall('rubric')
-            self.assertEqual(": ～な (<i>уст.</i> 筆忠実) хорошо владеющий пером", rubrics[0].text)
-            self.assertEqual(": ～な (<i>уст.</i> 筆忠実) любящий писать <i>(письма)</i>", rubrics[1].text)
+            self.assertEqual(": ～な (<i>уст.</i> 筆忠実) хорошо владеющий пером", rubrics[0][0].text)
+            self.assertEqual(": ～な (<i>уст.</i> 筆忠実) любящий писать <i>(письма)</i>", rubrics[1][0].text)
 
     def test_litter4(self):
         root = test_parse("005-57-66").getroot()
         for article in root:
             rubrics = article.findall('rubric')
-            self.assertEqual("(<i>сокр.</i> 軍事警察) военная полиция; жандармерия", rubrics[0].text)
-            self.assertEqual("(<i>сокр.</i> 軍事警察) военный полицейский; жандарм", rubrics[1].text)
+            self.assertEqual("(<i>сокр.</i> 軍事警察) военная полиция; жандармерия", rubrics[0][0].text)
+            self.assertEqual("(<i>сокр.</i> 軍事警察) военный полицейский; жандарм", rubrics[1][0].text)
 
     def test_litter5(self):
         root = test_parse("001-95-90").getroot()
         for article in root:
             rubrics = article.findall('rubric')
-            self.assertEqual("(<i>русск.</i> народники) народники", rubrics[0].text)
-            self.assertEqual("(<i>русск.</i> народники) народничество", rubrics[1].text)
+            self.assertEqual("(<i>русск.</i> народники) народники", rubrics[0][0].text)
+            self.assertEqual("(<i>русск.</i> народники) народничество", rubrics[1][0].text)
 
     def test_litter6(self):
         root = test_parse("005-75-60").getroot()
         for article in root:
             rubrics = article.findall('rubric')
-            self.assertEqual("<i>ономат.:</i> かんかん鳴る звенеть; лязгать", rubrics[0].text)
-            self.assertEqual("<i>ономат.:</i> かんかん日が照っている солнце светит ярко", rubrics[1].text)
-            self.assertEqual("<i>ономат.:</i> かんかんに怒る вспыхнуть от гнева, загореться гневом", rubrics[2].text)
+            self.assertEqual("<i>ономат.:</i> かんかん鳴る звенеть; лязгать", rubrics[0][0].text)
+            self.assertEqual("<i>ономат.:</i> かんかん日が照っている солнце светит ярко", rubrics[1][0].text)
+            self.assertEqual("<i>ономат.:</i> かんかんに怒る вспыхнуть от гнева, загореться гневом", rubrics[2][0].text)
 
     def test_litter7(self):
         root = test_parse("007-58-00").getroot()
         for article in root:
             rubrics = article.findall('rubric')
-            self.assertEqual(": ～する с почётом удалять (отстранять) <i>кого-л.</i>", rubrics[0].text)
+            self.assertEqual(": ～する с почётом удалять (отстранять) <i>кого-л.</i>", rubrics[0][0].text)
             self.assertEqual(": ～する держаться почтительно, но на расстоянии; относиться с уважением, но холодно",
-                             rubrics[1].text)
+                             rubrics[1][0].text)
 
     def test_litter8(self):
         root = test_parse("002-52-52").getroot()
         for article in root:
             rubrics = article.findall('rubric')
-            self.assertEqual("(に) сопровождать <i>кого-л.</i>; следовать <i>за чем-л.</i>", rubrics[0].text)
-            self.assertEqual("(に) вступать в брак, жениться, выходить замуж", rubrics[1].text)
+            self.assertEqual("(に) сопровождать <i>кого-л.</i>; следовать <i>за чем-л.</i>", rubrics[0][0].text)
+            self.assertEqual("(に) вступать в брак, жениться, выходить замуж", rubrics[1][0].text)
 
     def test_litter9(self):
         root = test_parse("005-78-01").getroot()
         for article in root:
             rubrics = article.findall('rubric')
-            self.assertEqual("(<i>нем.</i> Aphtha[e]) <i>мед.</i> молочница", rubrics[0].text)
-            self.assertEqual("(<i>нем.</i> Aphtha[e]) <i>мед.</i> афты", rubrics[1].text)
+            self.assertEqual("(<i>нем.</i> Aphtha[e]) <i>мед.</i> молочница", rubrics[0][0].text)
+            self.assertEqual("(<i>нем.</i> Aphtha[e]) <i>мед.</i> афты", rubrics[1][0].text)
 
     def test_litter10(self):
         root = test_parse("004-94-80").getroot()
         for article in root:
             rubrics = article.findall('rubric')
-            self.assertEqual(": ～な <i>прям. и перен.</i> нечистый, грязный", rubrics[0].text)
-            self.assertEqual(": ～な <i>прям. и перен.</i> смешанный, разбавленный", rubrics[1].text)
+            self.assertEqual(": ～な <i>прям. и перен.</i> нечистый, грязный", rubrics[0][0].text)
+            self.assertEqual(": ～な <i>прям. и перен.</i> смешанный, разбавленный", rubrics[1][0].text)
 
     def test_litter11(self):
         root = test_parse("006-07-86").getroot()
         for article in root:
             rubrics = article.findall('rubric')
             self.assertEqual("(<i>уст.</i> 襲ね) <i>счётный суф. для костюмов и различных комплектов</i>",
-                             rubrics[0].text)
-            self.assertEqual("(<i>уст.</i> 襲ね) <i>счётный суф. для слоёв; для полок и т. п.</i>", rubrics[1].text)
+                             rubrics[0][0].text)
+            self.assertEqual("(<i>уст.</i> 襲ね) <i>счётный суф. для слоёв; для полок и т. п.</i>", rubrics[1][0].text)
+
+
+class NestingTestsCase(unittest.TestCase):
+
+    # ちょうへん, ちょうへんしょうせつ【長篇･長編, 長篇小説･長編小説】(тё:хэн, тё:хэн-сё:сэцу)〔009-26-70〕
+    def test_nesting1(self):
+        root = test_parse("009-26-70").getroot()
+        self.assertEqual("ちょうへん", root[0].attrib['kana'])
+        self.assertEqual("ちょうへんしょうせつ", root[1].attrib['kana'])
+        self.assertEqual("тё:хэн", root[0].attrib['kiriji'])
+        self.assertEqual("тё:хэн-сё:сэцу", root[1].attrib['kiriji'])
+        hyoukies = root[0].findall('hyouki')
+        self.assertEqual("長篇", hyoukies[0].text)
+        self.assertEqual("長編", hyoukies[1].text)
+        hyoukies = root[1].findall('hyouki')
+        self.assertEqual("長篇小説", hyoukies[0].text)
+        self.assertEqual("長編小説", hyoukies[1].text)
+
+    # ちりぢり, ちりぢりばらばら【散々, 散々ばらばら】(тиридзири, тиридзири-барабара)〔005-50-45〕
+    def test_nesting2(self):
+        root = test_parse("005-50-45").getroot()
+        self.assertEqual("ちりぢり", root[0].attrib['kana'])
+        self.assertEqual("ちりぢりばらばら", root[1].attrib['kana'])
+        self.assertEqual("тиридзири", root[0].attrib['kiriji'])
+        self.assertEqual("тиридзири-барабара", root[1].attrib['kiriji'])
+        self.assertEqual("散々", root[0].findall('hyouki')[0].text)
+        self.assertEqual("散々ばらばら", root[1].findall('hyouki')[0].text)
+
+    # あいそ, あいそう【愛想】(айсо, айсо:)〔009-12-37〕
+    def test_nesting3(self):
+        root = test_parse("009-12-37").getroot()
+        self.assertEqual("あいそ", root[0].attrib['kana'])
+        self.assertEqual("あいそう", root[1].attrib['kana'])
+        self.assertEqual("айсо", root[0].attrib['kiriji'])
+        self.assertEqual("айсо:", root[1].attrib['kiriji'])
+        self.assertEqual("愛想", root[0].findall('hyouki')[0].text)
+        self.assertEqual("愛想", root[1].findall('hyouki')[0].text)
+
+    # てんでに, てんでんに(тэндэни, тэндэнни)〔000-16-88〕
+    def test_nesting4(self):
+        root = test_parse("000-16-88").getroot()
+        self.assertEqual("てんでに", root[0].attrib['kana'])
+        self.assertEqual("てんでんに", root[1].attrib['kana'])
+        self.assertEqual("тэндэни", root[0].attrib['kiriji'])
+        self.assertEqual("тэндэнни", root[1].attrib['kiriji'])
+
+    # ターボジェットき【ターボジェット機】(та:бодзеттоки, та:бодзйеттоки)〔008-07-89〕
+    def test_nesting5(self):
+        root = test_parse("008-07-89").getroot()
+        self.assertEqual("ターボジェットき", root[0].attrib['kana'])
+        self.assertEqual("та:бодзеттоки, та:бодзйеттоки", root[0].attrib['kiriji'])
+        self.assertEqual("ターボジェット機", root[0].findall('hyouki')[0].text)
+
+    # です(дэс, дэсў)〔006-48-38〕
+    def test_nesting6(self):
+        root = test_parse("006-48-38").getroot()
+        self.assertEqual("です", root[0].attrib['kana'])
+        self.assertEqual("дэс, дэсў", root[0].attrib['kiriji'])
+
+    # ボロ・タク, ぼろ・タク(боро-таку)〔007-29-08〕
+    def test_nesting7(self):
+        root = test_parse("007-29-08").getroot()
+        self.assertEqual("ボロ・タク", root[0].attrib['kana'])
+        self.assertEqual("ぼろ・タク", root[1].attrib['kana'])
+        self.assertEqual("боро-таку", root[0].attrib['kiriji'])
+        self.assertEqual("боро-таку", root[1].attrib['kiriji'])
 
 
 if __name__ == '__main__':
